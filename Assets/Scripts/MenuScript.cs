50d7e2e7a417b00a7c1ea10c77b4e601c6b52cb7
Carl Cai <shiyangcai12138@gmail.com>
2021-12-03 00:20:34 +0000

    No Comments

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string nextScene;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+        }
     }
 }
