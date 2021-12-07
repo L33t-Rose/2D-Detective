be73d680bccd820bc826597207a184604865d7b7
Carl Cai <shiyangcai12138@gmail.com>
2021-12-07 03:14:30 +0000

    No Comments

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
+    public int officeCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        officeCounter = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
